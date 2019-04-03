78de5cf0f2cf1b32785c98b044917db0f7fb4943
rafamorais <rafaelsilvademorais@gmail.com>
2019-04-03 20:58:13 -0300

    Aula operadores Concluida

--- app/operators.rb
+++ app/operators.rb
@@ -1,13 +1,35 @@
-var number1 = 0
-var number2 = 0
-var total = 0
+number1 = 0
+number2 = 1
 
-puts  "Digite o primeiro Número 1."
-number1 = STDIN.gets
-number1 = number1.to_i
+# puts number1 > number2
+# puts number1 < number2 + "\n"
+# puts number1 >= number2 + "\n"
+# puts number1 <= number2 + "\n"
+# puts number1 == number2 + "\n"
+# puts number1 != number2 + "\n"
+# puts number1.equal? number2 + "\n"
+# puts number1.eql? number2 + "\n"
+puts number1.to_i <=> number2.to_i
 
-puts  "Digite o primeiro Número 2."
-number2 = STDIN.gets
-number2 = number2.to_i
 
-puts total = number1 + number1
+# number1 = 0
+# number2 = 0
+# total = 0
+
+# puts  "Number 1"
+# number1 = STDIN.gets
+# number1 = number1.to_f
+
+# puts  "Number 2"
+# number2 = STDIN.gets
+# number2 = number2.to_f
+
+# total = number1 + number2
+# total = number1 - number2
+# total = number1 * number2
+# total = number1 / number2
+# total = number1 ** number2
+
+# puts "Result: " + total.to_s
+
+
