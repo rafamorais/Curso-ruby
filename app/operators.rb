542deafba4d73d28d7bb08876e8593611e224864
rafamorais <rafaelsilvademorais@gmail.com>
2019-04-03 19:44:26 -0300

    adicionando operators

--- app/operators.rb
+++ app/operators.rb
@@ -1,2 +1,13 @@
 var number1 = 0
 var number2 = 0
+var total = 0
+
+puts  "Digite o primeiro Número 1."
+number1 = STDIN.gets
+number1 = number1.to_i
+
+puts  "Digite o primeiro Número 2."
+number2 = STDIN.gets
+number2 = number2.to_i
+
+puts total = number1 + number1
