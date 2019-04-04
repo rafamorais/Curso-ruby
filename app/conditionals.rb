d8282edad7ae83f9ad261a0090aada0fd704f7d7
rafamorais <rafaelsilvademorais@gmail.com>
2019-04-03 21:38:20 -0300

    Condicionais finalizados

---
+++ app/conditionals.rb
@@ -0,0 +1,81 @@
+$age = 0
+$name = ''
+$reuslt = ''
+
+# puts  "What is your name?"
+# $name = STDIN.gets
+
+# puts  "How old are you?"
+# $age = STDIN.gets.to_i
+
+# if $age >= 20
+#     puts 'To Old'
+# end
+
+# if $age < 20
+#     puts 'To young'
+# end
+
+# if ($age >= 20)
+#     puts 'To old'
+# else
+#     puts 'To young'
+# end
+
+# $reuslt = if ($age >= 20)
+#             'To old'
+#           else
+#             'To young'
+#           end
+# puts $reuslt
+
+# // Operador Ternario
+# $reuslt = ($age >= 20) ? 'To old' : 'To young'
+
+# puts $reuslt
+
+# if $age > 20
+#     puts 'Maior que 20'
+# elsif $age < 20
+#     puts 'Menor que 20'
+# else
+#     puts 'Igual a 20'
+# end
+
+# $car_is_on = false
+
+# if not $car_is_on
+#     puts "off"
+# end
+
+# $car_is_on = false
+
+# unless $car_is_on
+#     puts "off"
+# end
+
+# $week_day = 1
+
+# puts  "Dia da Semana"
+# $week_day = STDIN.gets.to_i
+
+# case $week_day
+
+#     when $week_day = 1
+#         puts 'Segunda'
+#     when $week_day = 2
+#         puts 'Terça'
+#     when $week_day = 3
+#         puts 'Quarta'
+#     when $week_day = 4
+#         puts 'Quinta'        
+#     when $week_day = 5
+#         puts 'Sexta'    
+#     when $week_day = 6
+#         puts 'Sábado'  
+#     when $week_day = 0
+#         puts 'Domingo'
+#     else
+#         puts 'Nenhum dia da semana selecionado'   
+
+# end
