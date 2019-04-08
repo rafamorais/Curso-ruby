d7c01c91c4fcfa1e19f2411b0682721f93f934c4
rafamorais <rafaelsilvademorais@gmail>
2019-04-08 09:16:09 -0300

    finalizando loops

---
+++ app/loops.rb
@@ -0,0 +1,47 @@
+$init = 0
+$max = 10
+
+# while $init < $max
+#     $init += 1
+#     puts 'iteration ' + $init.to_s + "\n"
+    
+# end
+
+# while $init < $max do
+#     $init += 1
+#     puts 'iteration ' + $init.to_s + "\n"
+    
+# end
+
+# begin
+#     $init += 1
+#     puts 'iteration ' + $init.to_s + "\n"
+# end while $init < $max 
+
+# teachers  = Array.[]('Rafa Morais', 'Neo', 'Regis')
+
+# for value in teachers do
+#     puts value
+# end
+
+# for $init in 0..10
+#     puts $init + 2
+# end
+
+$arr = ['item 1', 'item 2', 'item 3', 'item 4']
+
+# item = 0
+# until item > $arr.length do
+
+#     puts $arr[item]
+#     item += 1
+    
+# end
+
+$arr.each do | b |
+    puts b
+end    
+
+# for value in $arr do
+#     puts value
+# end
